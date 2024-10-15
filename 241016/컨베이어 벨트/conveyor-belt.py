6db2825c48e7595d3ccd5a16bbb01c8639ac0775
IdontknowTT <121224370+IdontknowTT@users.noreply.github.com>
2024-10-16 00:48:37 +0900

    Title: 컨베이어 벨트, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

--- 241016/컨베이어 벨트/conveyor-belt.py
+++ 241016/컨베이어 벨트/conveyor-belt.py
@@ -6,6 +6,7 @@ down = list(map(int, input().split())) #아래 리스트 받기
 
 #둘다 큐로 바꿔준다
 up = deque(up)
+down = down[::-1]
 down = deque(down)
 
 
@@ -17,6 +18,13 @@ for _ in range(t):
     up.insert(0,temp2)
     down.append(temp1)
 
-    print(*)
-    print(up)
-    print(down)
+
+up = list(up)
+down = list(down)
+down = down[::-1]
+
+for i in up:
+    print(i, end = " ")
+print()
+for i in down:
+    print(i, end = " ")
